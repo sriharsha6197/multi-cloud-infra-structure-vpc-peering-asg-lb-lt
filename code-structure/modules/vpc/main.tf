debd042a29caf008602fa519bc0cbe2fa4a3ae92
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-10 19:25:26 +0530

    create index value using zipmap function

--- code-structure/modules/vpc/main.tf
+++ code-structure/modules/vpc/main.tf
@@ -5,11 +5,11 @@ resource "aws_vpc" "vpc" {
   }
 }
 resource "aws_subnet" "public_subnets" {
-  for_each = var.public_subnets
+  for_each = zipmap(range(length(var.public_subnets)),var.public_subnets)
   vpc_id     = aws_vpc.vpc.id
   cidr_block = each.value
 
   tags = {
-    Name = "${var.env}-public-subnet-${range(length(var.public_subnets)) + 1}"
+    Name = "${var.env}-public-subnet-${each.key + 1}"
   }
 }
