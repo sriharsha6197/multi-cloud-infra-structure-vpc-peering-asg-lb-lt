6b50b0d311d0ba57271f84b85e1c3d31a3e4ce26
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-10 19:16:41 +0530

    i

--- code-structure/modules/vpc/main.tf
+++ code-structure/modules/vpc/main.tf
@@ -10,6 +10,6 @@ resource "aws_subnet" "public_subnets" {
   cidr_block = each.value
 
   tags = {
-    Name = "${var.env}-public-subnet-${index(var.public_subnets, each.value) + 1}"
+    Name = "${var.env}-public-subnet-${range(length(var.public_subnets)) + 1}"
   }
 }
