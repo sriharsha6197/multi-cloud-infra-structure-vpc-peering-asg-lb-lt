54aa26d7f7d3f6e8b59bfdc31ce7f34b6e9cf77d
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-12 21:02:13 +0530

    lb creation

--- code-structure/modules/vpc/output.tf
+++ code-structure/modules/vpc/output.tf
@@ -1,15 +1,9 @@
 output "public_subnets" {
-  value = aws_subnet.public_subnets[*]
+  value = aws_subnet.public_subnets.*.id
 }
 output "vpc_CIDR" {
   value = var.vpc_cidr
 }
 output "pb_rt_cidr_block" {
   value = var.public_rt_cidr_block
-}
-output "from_PORT" {
-  value = var.from_port.*
-}
-output "to_PORT" {
-  value = var.to_port.*
 }
