4f5460d7d146da47d2cff967ab0ba5d458a4f3c4
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-13 13:05:46 +0530

    sdf

--- code-structure/modules/vpc/output.tf
+++ code-structure/modules/vpc/output.tf
@@ -1,6 +1,9 @@
-output "subnet_ids" {
+output "pb_subnets" {
   value = values(resource.aws_subnet.public_subnet)[*].id
 }
 output "vpc_CIDR_ID" {
   value = aws_vpc.vpc.id
+}
+output "pvt_subnets" {
+  value = values(resouce.aws_subnet.private_subnets)[*].id
 }
