1de8fc21b7779106e7ef2db40fcfda6a83730277
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-13 17:57:29 +0530

    hg

--- code-structure/main.tf
+++ code-structure/main.tf
@@ -9,15 +9,15 @@ module "vpc" {
   private_subnets = var.private_subnets
   public_lb_azs = var.public_lb_azs
 }
-# module "alb" {
-#   source = "./modules/lb"
-#   for_each = var.alb_type_internal
-#   alb_type = each.value
-#   internal = each.key
-#   vpc_id = module.vpc.vpc_ID
-#   env = var.env
-#   lb_public_cidr_block = var.public_rt_cidr_block
-#   SUBNETS = each.key == false ? module.vpc.PUBLIC_SUBNETS : module.vpc.PRIVATE_SUBNETS
-#   from_port = var.from_port
-#   to_port = var.to_port
-# }
+module "alb" {
+  source = "./modules/lb"
+  for_each = var.alb_type_internal
+  alb_type = each.value
+  internal = each.key
+  vpc_id = module.vpc.vpc_ID
+  env = var.env
+  lb_public_cidr_block = var.public_rt_cidr_block
+  SUBNETS = each.key == false ? module.vpc.PUBLIC_SUBNETS : module.vpc.PRIVATE_SUBNETS
+  from_port = var.from_port
+  to_port = var.to_port
+}
