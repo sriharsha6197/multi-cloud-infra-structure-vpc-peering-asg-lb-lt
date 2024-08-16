aea38e10abb5888d77b0f1b2d62d2108637867b6
Sriharsha6197 <m.sriharsha97@gmail.com>
2024-08-16 19:25:49 +0530

    redirecting out[put logs in a file

--- code-structure/modules/lt/app_config.sh
+++ code-structure/modules/lt/app_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-dnf list all | grep python
-dnf install python3.12-pip.noarch -y
-pip3.12 install botocore boto3 -y
-dnf install ansible -y
-ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e role_name=${server_component}
+dnf list all | grep python | tee -a /opt/output.log
+dnf install python3.12-pip.noarch -y | tee -a /opt/output.log
+pip3.12 install botocore boto3 - | tee -a /opt/output.log
+dnf install ansible -y | tee -a /opt/output.log
+ansible-pull -i localhost, -U https://github.com/sriharsha6197/expense-ansible.git -e ansible_user=centos -e ansible_password=DevOps321 -e role_name=${server_component} | tee -a /opt/output.log
